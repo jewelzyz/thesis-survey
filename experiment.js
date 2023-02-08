6d6defa79c4d04ff5482b33bd506714b29b61761
jewelzyz <46210190+jewelzyz@users.noreply.github.com>
2023-02-08 09:22:04 -0800

    updates for html

--- experiment.js
+++ experiment.js
@@ -1,7 +1,5 @@
 var trial_counter = 0;
-var exp;
 function slide_set() {
-
 }
 
 // set up experiment logic for each slide
@@ -15,55 +13,41 @@ function make_slides(f) {
       exp.startT = Date.now();
     }
   });
-
   // set up the first example slide
-  slides.example1 = slide({
-    name: "example1",
+  slides.instructions = slide({
+    name: "instructions",
 
     // this is executed when the slide is shown
     start: function() {
       // hide error message
       $('.err').hide();
-    },
+      $('example_buttons').hide();
+    }
+  });
 
-    // this is executed when the participant clicks the "Continue button"
-    button: function() {
-      // read in the value of the selected radio button
-      this.radio = $("input[name='number']:checked").val();
-      // check whether the participant selected a reasonable value (i.e, 5, 6, or 7)
-      if (this.radio) {
-        // log response
-        this.log_responses();
-        // continue to next slide
-        exp.go();
-      } else {
-        // participant gave non-reasonable response --> show error message
-        $('.err').show();
-        this.log_responses();
-      }
-    },
-
-    log_responses: function() {
+//    log_responses: function() {
       // add response to exp.data_trials
       // this data will be submitted at the end of the experiment
-      exp.data_trials.push({
-        "slide_number_in_experiment": exp.phase,
-        "id": "example1",
-        "response": this.radio,
+  //    exp.data_trials.push({
+    //    "slide_number_in_experiment": exp.phase,
+      //  "id": "instructions",
+        //"response": this.radio,
         // "strangeSentence": "",
-      });
-    },
-  });
+//      });
+  //  },
+//  });
 
-  // set up slide with instructions for main experiment
-  slides.startExp = slide({
-    name: "startExp",
-    start: function() {
-    },
-    button: function() {
-      exp.go(); //use exp.go() if and only if there is no "present" data.
-    },
-  });
+slides.example = slide({
+  name: "example",
+
+  start: function() {
+    // hide error message and buttons
+    $('.err').hide();
+    $('example_buttons').hide();
+    document.getElementById("example_buttons").style.display = "none";
+  }
+
+});
 
   slides.trial = slide({
     name: "trial",
@@ -72,78 +56,31 @@ function make_slides(f) {
     present: exp.stimuli,
     present_handle : function(stim) {
 
-      // unselect all radio buttons at the beginning of each trial
-      // (by default, the selection of the radio persists across trials)
-      $("input[name='number']:checked").prop("checked", false);
-      $("input[name='familiarity']:checked").prop("checked", false);
-      $("#check-strange").prop("checked", false);
-
       // store stimulus data
       this.stim = stim;
-
-      exp.item = exp.gender.pop()
-      if (exp.item == "male") {
-        var title_frame = stim.male;
-        this.orth = stim.male_orthog;
-      } else if (exp.item == "female") {
-        var title_frame = stim.female;
-        this.orth = stim.female_orthog;
-      } else {
-        var title_frame = stim.neutral;
-        this.orth = stim.neutral_orthog;
-      }
+      //exp.stimuli = _.shuffle(stimuli); //call _.shuffle(stimuli) to randomize the order;
+      exp.item = exp.stimuli.pop()
 
 
       console.log('item',exp.item)
 
       // replace the placeholder in the HTML document with the relevant sentences for this trial
-      $("#trial-title_frame").html(title_frame);
+      $("#stimulus-sentence").html(exp.item);
       $(".err").hide();
-    },
-
-    // handle click on "Continue" button
-    button: function() {
-      this.radio = $("input[name='number']:checked").val();
-      this.checkbox = $("input[name='familiarity']:checked").val();
-      if (this.radio) {
-        this.log_responses();
-        _stream.apply(this); //use _stream.apply(this) if there is a list of "present" stimuli to rotate through
-      } else {
-        $('.err').show();
-      }
     },
 
     // save response
     log_responses: function() {
       exp.data_trials.push({
         "id": this.stim.ID,
-        "gender": exp.item,
-        "scale": exp.scale,
-        "slide_number_in_experiment": exp.phase, //exp.phase is a built-in trial number tracker
-        "response": this.radio,
-        "familiarity": this.checkbox,
-        "gendered_morph": this.stim.relationship,
-        "neutral_morh": this.stim.neutral_form,
-        "orthog": this.orth,
+        "legal": this.stim.ID,
+        "response": exp.text_answer, //textbox input
+        "classification": exp.answer //'object', 'opportunity', 'neither/both'
       });
     },
-  });
 
-  // slide to collect subject information
-  slides.subj_info = slide({
-    name: "subj_info",
-    submit: function(e) {
-      exp.subj_data = {
-        language: $("#language").val(),
-        enjoyment: $("#enjoyment").val(),
-        asses: $('input[name="assess"]:checked').val(),
-        age: $("#age").val(),
-        gender: $("#gender").val(),
-        education: $("#education").val(),
-        fairprice: $("#fairprice").val(),
-        comments: $("#comments").val()
-      };
-      exp.go(); //use exp.go() if and only if there is no "present" data.
+    start: function() {
+      document.getElementById("buttons").style.display = "none";
     }
   });
 
@@ -165,67 +102,44 @@ function make_slides(f) {
   return slides;
 }
 
+function show_example_buttons() {
+  var x = document.getElementById("example_buttons");
+  x.style.display = "block";
+
+}
+
+function show_buttons() {
+    var y = document.getElementById("buttons");
+    y.style.display = "block";
+}
+
 /// initialize experiment
 function init() {
 
   exp.trials = [];
-  exp.catch_trials = [];
-  var stimuli = all_stims;
+  //exp.catch_trials = [];
+  var stimuli = {to: ["to"], from: ["from"], of: ["of"]}
 
   exp.stimuli = stimuli;
   exp.stimuli = _.shuffle(stimuli); //call _.shuffle(stimuli) to randomize the order;
-  exp.n_trials = exp.stimuli.length;
-
-
-  exp.gender = _.shuffle(['male','male','male','male','male','male','male','male','male','male','male','male','male','female','female','female','female','female','female','female','female','female','female','female','female','female','neutral','neutral','neutral','neutral','neutral','neutral','neutral','neutral','neutral','neutral','neutral','neutral','neutral']);
-
-  /// Randomising the scale for each participant
-  var exp_scale = _.shuffle(["MF", "FM"])
-  exp.scale = exp_scale.pop()
-  if (exp.scale == "MF") {
-    var scale_end_one = "very likely a man";
-    var scale_end_two = "very likely a woman";
-  } else {
-    var scale_end_one = "very likely a woman";
-    var scale_end_two = "very likely a man";
-  }
-  $("#scale_end_one").html(scale_end_one);
-  $("#scale_end_two").html(scale_end_two);
-
-
-  exp.system = {
-    Browser: BrowserDetect.browser,
-    OS: BrowserDetect.OS,
-    screenH: screen.height,
-    screenUH: exp.height,
-    screenW: screen.width,
-    screenUW: exp.width
-  };
+  exp.n_trials = 10;
 
   //blocks of the experiment:
   exp.structure = [
     "i0",
-    "example1",
-    "startExp",
+    "instructions",
+    "example",
+    "almost",
     "trial",
-    "subj_info",
     "thanks"
   ];
 
-  exp.data_trials = [];
+  //exp.data_trials = [];
 
   //make corresponding slides:
   exp.slides = make_slides(exp);
 
-  exp.nQs = utils.get_exp_length();
-  //this does not work if there are stacks of stims (but does work for an experiment with this structure)
-  //relies on structure and slides being defined
-
   $('.slide').hide(); //hide everything
-
-  $("#start_button").click(function() {
-    exp.go();
-  });
 
   exp.go(); //show first slide
 }
